50c62bf123c68dd9223bece432f491e079f1c180
Pete Pham <php.statistics@gmail.com>
2020-05-22 15:23:51 -0700

    start

---
+++ basecode.R
@@ -0,0 +1,36 @@
+# Abstract (Summary) ###################
+## Background
+## Objective
+## Methods
+## Results
+## Conclusion
+
+# Introduction ###################
+## Background
+
+## Objective
+
+## Data
+### Table of the data
+### Header definition
+
+## Definitions
+### Medical definitions
+### Censored information
+### Randomness
+
+
+# Methods ###################
+## Statistical Analysis
+### Kaplan-Meier
+### Weibull
+### Cox PH
+
+# Results ###################
+## Descriptive Statistics
+## Survival Analysis
+## Model Dianostics
+
+# Conclusion
+
+# Discussion
